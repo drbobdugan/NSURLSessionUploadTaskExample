74f26acfc61debd9e0af9c4559b5babd74aaacda
drbobdugan <bdugan@stonehill.edu>
2015-10-26 10:20:30 -0400

    Initial Commit

---
+++ NSURLSessionUploadExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NSURLSessionUploadExample
+//
+//  Created by Bob Dugan on 10/26/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NSURLSessionUploadExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSURLSessionUploadExample
+//
+//  Created by Bob Dugan on 10/26/15.
+//  Copyright © 2015 Bob Dugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
